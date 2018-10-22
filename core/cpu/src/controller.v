5d344b846c405a879f2a3c9cc02345feae3bcd90
Yosshi999 <Yosshi999@example.com>
2018-10-22 18:33:04 +0900

    float

--- core/cpu/src/controller.v
+++ core/cpu/src/controller.v
@@ -6,8 +6,13 @@
 `define OP_slwi     6'd5
 `define OP_srwi     6'd6
 
-`define OP_slw      6'd7  // temporal
-`define OP_srw      6'd8  // temporal
+`define OP_fadd     6'd7
+`define OP_fsub     6'd8
+`define OP_fdiv     6'd9 // not implemented
+`define OP_fmul     6'd10
+`define OP_fsqrt    6'd11 // not implemented
+`define OP_fabs     6'd12 // not implemented
+`define OP_fmr       6'd13
 
 `define OP_b        6'd14
 `define OP_beq      6'd15
@@ -21,9 +26,12 @@
 
 `define OP_cmpwi    6'd22
 `define OP_cmpw     6'd23
+`define OP_fcmpw    6'd24
 
-`define OP_ld       6'b011001
-`define OP_st       6'b011010
+`define OP_ld       6'd25
+`define OP_st       6'd26
+`define OP_fld      6'd27
+`define OP_fst      6'd28
 
 
 `define OP_in      6'b111110
@@ -47,6 +55,11 @@
 `define A_COM_sll 4'd3
 `define A_COM_srl 4'd4
 
+`define FA_COM_nop 4'd0
+`define FA_COM_add 4'd1
+`define FA_COM_sub 4'd2
+`define FA_COM_mul 4'd3
+
 `define ERR_C_INVALID_STATE 8'b1000_0000
 `define ERR_C_INVALID_OP    8'b0100_0000
 `define ERR_C_HALT          8'b0000_0001
@@ -56,27 +69,50 @@ module Alu(
   input wire [31:0] rA,
   input wire [31:0] rB,
   input wire [3:0] command,
-  output wire [31:0] rD
+  output reg [31:0] rD,
+
+  input wire clk,
+  input wire rstn
 );
-  function [31:0] out;
-    input [31:0] a;
-    input [31:0] b;
-    input [3:0] com;
-
-    begin
-      case (command)
-        `A_COM_nop: out = a;
-        `A_COM_add: out = a + b;
-        `A_COM_sub: out = a - b;
-        `A_COM_sll: out = a << b;
-        `A_COM_srl: out = a >> b;
-        `A_COM_sub: out = a - b;
-        default: out = 0;
-      endcase
+  always @(posedge clk) begin
+    if (~rstn) begin
+      rD <= 32'd0;
+    end else begin
+      if (command == `A_COM_nop) rD <= 32'd0;
+      if (command == `A_COM_add) rD <= rA + rB;
+      if (command == `A_COM_sub) rD <= rA - rB;
+      if (command == `A_COM_sll) rD <= rA << rB;
+      if (command == `A_COM_srl) rD <= rA >> rB;
     end
-  endfunction
+  end
+endmodule
 
-  assign rD = out(rA, rB, command);
+module FAlu (
+  input wire [31:0] x1,
+  input wire [31:0] x2,
+  output wire [31:0] y,
+  output wire ovf,
+  input wire [3:0] command,
+
+  input wire clk,
+  input wire rstn);
+
+  wire [31:0] ya, ys, ym;
+  wire ovfa, ovfs, ovfm;
+  fadd_p2 adder (x1, x2, ya, ovfa, clk, rstn);
+  fsub_p2 subber(x1, x2, ys, ovfs, clk, rstn);
+  fmul    muller(x1, x2, ym, ovfm);
+
+  assign y = (
+    (command == `FA_COM_add) ? ya :
+    (command == `FA_COM_sub) ? ys :
+    (command == `FA_COM_mul) ? ym : 32'b0);
+
+  assign ovf = (
+    ((command == `FA_COM_add) & ovfa) |
+    ((command == `FA_COM_sub) & ovfs) |
+    ((command == `FA_COM_mul) & ovfm));
+
 endmodule
 
 module Controller(
@@ -103,11 +139,12 @@ module Controller(
   input wire CLK,
   input wire RSTN
 );
-  
+  // program counter
   reg [31:0] pc;
   reg [31:0] jump_pc;
   reg will_jump;
 
+  // instruction
   assign mem_inst_addr = {pc[29:0], 2'b00};
   assign mem_inst_enable = 1'b1;
   wire [5:0] inst_op;
@@ -122,6 +159,7 @@ module Controller(
   assign inst_imm16 = inst[16:31];
   assign inst_imm26 = inst[6:31];
 
+  // register
   reg reg_we;
   reg [31:0] reg_wdata;
   wire [31:0] rA;
@@ -130,20 +168,41 @@ module Controller(
   RegisterFile register(inst_rD, inst_rA, inst_rB, rD, rA, rB, 
     reg_we, reg_wdata, CLK);
 
+  // float register
+  reg freg_we;
+  reg [31:0] freg_wdata;
+  wire [31:0] frA;
+  wire [31:0] frB;
+  wire [31:0] frD;
+  RegisterFile fregister(inst_rD, inst_rA, inst_rB, frD, frA, frB, 
+    freg_we, freg_wdata, CLK);
+
+  // condition register
   reg [0:3] condreg;
   wire c0_lt, c0_gt, c0_eq, c0_of;
   assign {c0_lt, c0_gt, c0_eq, c0_of} = condreg;
 
+  // link register
   reg [31:0] linkreg;
 
+  // alu
   reg [3:0] command;
   reg use_imm;
   reg [31:0] imm;
   wire [31:0] alu_in0, alu_in1, alu_out;
   assign alu_in0 = rA;
   assign alu_in1 = use_imm ? imm : rB;
-  Alu int_alu(alu_in0, alu_in1, command, alu_out);
+  Alu int_alu(alu_in0, alu_in1, command, alu_out, CLK, RSTN);
 
+  // float alu
+  reg [3:0] fcommand;
+  wire [31:0] falu_in0, falu_in1, falu_out;
+  assign falu_in0 = frA;
+  assign falu_in1 = frB;
+  wire fovf;
+  FAlu float_alu(falu_in0, falu_in1, falu_out, fovf, fcommand, CLK, RSTN);
+
+  // signed immediate
   function [31:0] simm16_32 (
     input [0:15] simm16
   );
@@ -156,6 +215,7 @@ module Controller(
     simm26_32 = simm26[0] ? {6'b111111, simm26} : {6'h0, simm26};
   endfunction
 
+  // status
   localparam [2:0] s_init   = 3'b000;
   localparam [2:0] s_fetch  = 3'b001;
   localparam [2:0] s_decode = 3'b010;
@@ -206,6 +266,7 @@ module Controller(
           will_jump <= 0;
           use_imm <= 1'b0;
           reg_we <= 1'b0;
+          freg_we <= 1'b0;
         end
 ///////////////////////////////////////////////////
         s_decode: begin
@@ -243,12 +304,25 @@ module Controller(
               imm <= inst_rB;
             end
 
-            `OP_slw: begin
-              command <= `A_COM_sll;
+            // `OP_slw: begin
+            //   command <= `A_COM_sll;
+            // end
+
+            // `OP_srw: begin
+            //   command <= `A_COM_srl;
+            // end
+
+            `OP_fadd: begin
+              fcommand <= `FA_COM_add;
+            end
+            `OP_fsub: begin
+              fcommand <= `FA_COM_sub;
+            end
+            `OP_fmul: begin
+              fcommand <= `FA_COM_mul;
             end
 
-            `OP_srw: begin
-              command <= `A_COM_srl;
+            `OP_fmr: begin
             end
 
             `OP_b: begin
@@ -296,6 +370,9 @@ module Controller(
             `OP_cmpw: begin
               command <= `A_COM_sub;
             end
+            `OP_fcmpw: begin
+              fcommand <= `FA_COM_sub;
+            end
 
             `OP_ld: begin
               mem_addr <= rA + simm16_32(inst_imm16);
@@ -304,6 +381,16 @@ module Controller(
             `OP_st: begin
               mem_addr <= rA + simm16_32(inst_imm16);
               mem_wdata <= rD;
+              mem_enable <= 1'b1;
+              mem_wenable <= 4'b1111;
+            end
+            `OP_fld: begin
+              mem_addr <= rA + simm16_32(inst_imm16);
+              mem_enable <= 1'b1;
+            end
+            `OP_fst: begin
+              mem_addr <= rA + simm16_32(inst_imm16);
+              mem_wdata <= frD;
               mem_enable <= 1'b1;
               mem_wenable <= 4'b1111;
             end
@@ -332,6 +419,15 @@ module Controller(
               mem_enable <= 1'b0;
               mem_wenable <= 1'b0;
             end
+            `OP_fld: begin
+              mem_enable <= 1'b0;
+            end
+            `OP_fst: begin
+              mem_enable <= 1'b0;
+              mem_wenable <= 1'b0;
+            end
+            default: begin
+            end
           endcase
           state <= s_write;
         end
@@ -350,9 +446,17 @@ module Controller(
             reg_wdata <= rA;
             reg_we <= 1'b1;
           end
+          if (inst_op == `OP_fmr) begin
+            freg_wdata <= rA;
+            freg_we <= 1'b1;
+          end
           if ((inst_op >= `OP_addi) && (inst_op <= `OP_srwi)) begin
             reg_wdata <= alu_out;
             reg_we <= 1'b1;
+          end
+          if ((inst_op >= `OP_fadd) && (inst_op <= `OP_fsqrt)) begin
+            freg_wdata <= falu_out;
+            freg_we <= 1'b1;
           end
           if (inst_op == `OP_mflr) begin
             reg_wdata <= linkreg;
@@ -364,9 +468,16 @@ module Controller(
           if (inst_op == `OP_cmpw) begin
             condreg <= {alu_out[31], ~(alu_out[31]), ~(|alu_out), 1'b0};
           end
+          if (inst_op == `OP_fcmpw) begin
+            condreg <= {falu_out[31], ~(falu_out[31]), ~(falu_out[30:23]==0), 1'b0};
+          end
           if (inst_op == `OP_ld) begin
             reg_wdata <= mem_rdata;
             reg_we <= 1'b1;
+          end
+          if (inst_op == `OP_fld) begin
+            freg_wdata <= mem_rdata;
+            freg_we <= 1'b1;
           end
 
           state <= s_fetch;
